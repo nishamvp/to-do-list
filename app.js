d5e118a044d211d2d7ca62e8a4e0add4396ff159
nishamvp <nishamvp30@gmail.com>
2022-09-24 18:08:34 +0530

    password changed in mongo url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("Public"));
 
-mongoose.connect("mongodb+srv://nishamvp:Nisham@1999@cluster0.hgzksnk.mongodb.net/todolistDB")
+mongoose.connect("mongodb+srv://nishamvp:nisham123@cluster0.hgzksnk.mongodb.net/todolistDB")
 
 const itemsSchema= new mongoose.Schema({
     name:String
